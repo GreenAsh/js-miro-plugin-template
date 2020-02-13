310d3d098cdb298c0e03b27897b744ba5d11fbb5
GreenAsh <moskalenkoip@gmail.com>
2020-02-13 20:27:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,16 @@ miro.onReady(async () => {
               if (!widgets || !widgets.length || widgets.length !== 1){
                 return [];
               }
-              
-              const nearestWidgets = await findNearestWidgets(widgets[0]);
               const widget = widgets[0];
               
-              const upClick = nearestWidgets.next === false ? false : async (widgets) => {
+              const upClick = async (widgets) => {
                 const uwidgets = await findNearestWidgets(widget)
                 if (uwidgets.next !== false) {
                   await miro.board.figma.moveFront(widget, uwidgets.next);
                 }
               }
               
-              const downClick = nearestWidgets.prev === false ? false : async (widgets) => {
+              const downClick = async (widgets) => {
                 const dwidgets = await findNearestWidgets(widget)
                 if (dwidgets.prev !== false) {
                   await miro.board.figma.moveBack(widget, dwidgets.prev);
