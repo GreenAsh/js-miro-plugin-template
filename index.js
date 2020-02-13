2b11bbd2f4baba853e808c52039cccefce841631
GreenAsh <moskalenkoip@gmail.com>
2020-02-13 20:40:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const up_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"/></svg>`;
+const down_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/></svg>`;
 
-const down_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/></svg>`
+const up_disabled_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path style="fill: #cccccc;"  d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"/></svg>`;
+const down_disabled_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path style="fill: #cccccc;" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/></svg>`
   
 miro.onReady(async () => {
     const authorized = await miro.isAuthorized();
@@ -16,15 +18,16 @@ miro.onReady(async () => {
                 return [];
               }
               const widget = widgets[0];
+              const nearestWidgets = await findNearestWidgets(widget)
               
-              const upClick = async (widgets) => {
+              const upClick = nearestWidgets.next === false ? () => {} : async (widgets) => {
                 const uwidgets = await findNearestWidgets(widget)
                 if (uwidgets.next !== false) {
                   await miro.board.figma.moveFront(widget, uwidgets.next);
                 }
               }
               
-              const downClick = async (widgets) => {
+              const downClick = nearestWidgets.prev === false ? () => {} : async (widgets) => {
                 const dwidgets = await findNearestWidgets(widget)
                 if (dwidgets.prev !== false) {
                   await miro.board.figma.moveBack(widget, dwidgets.prev);
@@ -33,11 +36,11 @@ miro.onReady(async () => {
               
                 return [{
                     tooltip: 'Up',
-                    svgIcon: up_icon,
+                    svgIcon: nearestWidgets.next === false ? up_disabled_icon : up_icon,
                     onClick: upClick
                 }, {
                     tooltip: 'Down',
-                    svgIcon: down_icon,
+                    svgIcon: nearestWidgets.prev === false ? down_disabled_icon : down_icon,
                     onClick: downClick
                 }];
             }
