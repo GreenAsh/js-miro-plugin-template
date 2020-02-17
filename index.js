d4c47ab587e3032785663a4a8472fcf20833724f
GreenAsh <moskalenkoip@gmail.com>
2020-02-17 14:47:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
-miro.onReady(async () => {
-// check if user is authorized these plugin
-//     const authorized = await miro.isAuthorized();
-//     if (!authorized) {
-//         return;
-//     }
-
-//     await miro.initialize({
-//         extensionPoints: {
-//             toolbar: {
-//                 title: 'string',
-//                 toolbarSvgIcon: 'string',
-//                 librarySvgIcon: 'string',
-//                 onClick: () => {
-//                     //do something
-//                 }
-//             },
-//             bottomBar: {
-//                 title: 'string',
-//                 toolbarSvgIcon: 'string',
-//                 librarySvgIcon: 'string',
-//                 onClick: () => {
-//                     //do something
-//                 }
-//             },
-//             exportMenu: {
-//                 title: 'string',
-//                 toolbarSvgIcon: 'string',
-//                 librarySvgIcon: 'string',
-//                 onClick: () => {
-//                     //do something
-//                 }
-//             },
-//             getWidgetMenuItems: async (widgets) => {
-//                 return [{
-//                     tooltip: 'string'
-//             	    svgIcon: 'string',
-//             	    onClick: () => {
-//                         //do something
-//                     }
-//                 }];
-//             }
-//         }
-//     })
-});
+miro.onReady( async () => {
+    const plugin_name = 'dummy-plugin'
+    const clientId = await miro.getClientId()
+    
+    window.addEventListener("message", async (e) => {
+        console.log(`${plugin_name} | ${clientId} | ${JSON.stringify(e.data)}`)
+    })
+} );
